1ed5322c95b42e61a39c8936122bb2e60b5f1895
ghostrider070008 <ghostrider07008@yandex.ru>
2019-03-06 21:34:02 +0300

    Выполнены первые три задания

--- zadanie3.php
+++ zadanie3.php
@@ -1,24 +1,19 @@
 <?
-function sum($x,$y){
-    $z = $x + $y;
-    return $z;
+$i = 0;
+$obl = ['Московская Область' => ['Москва','Зеленоград','Клин'], 'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],'Рязанская область' => ['Рязань','Шацк','Шилово','Сасово','Касимов']];
+foreach ($obl as $key => $value){
+    echo "$key:<br>";
+    $i=0;
+    $counter = count($value)-1;
+    foreach($value as $val){
+        if ($i==$counter){
+            echo "$val";
+            
+            }
+        else{
+        echo "$val, ";
+    }
+    $i++;
+    }
+    echo "<br>";
 }
-function dif($x,$y){
-    $z = $x - $y;
-    return $z;
-}
-
-function mult($x,$y){
-    $z = $x * $y;
-    return $z;
-}
-
-function div($x,$y){
-    $z = $x / $y;
-    return $z;
-}
-
-echo ('Операция сложения равна x + y ='.sum(25,5).'</br>');
-echo ('Операция вычитания равна x - y ='.dif(19,10).'</br>');
-echo ('Операция умножения равна x * y ='.mult(15,26).'</br>');
-echo ('Операция деления равна x / y ='.div(125,5).'</br>');

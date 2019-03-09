b5c2c04748a0b15cb44a319243e31b7fcee17d15
ghostrider070008 <ghostrider07008@yandex.ru>
2019-03-09 12:44:15 +0300

    Создание ДЗ 4 урока

--- zadanie5.php
+++
@@ -1,9 +0,0 @@
-<?
-$str = 'Самосвал въехал в гору';
-function replace_string($string) {
-    $converter = array(
-        ' ' => '_'
-    );
-    return strtr($string, $converter);
-}
-echo replace_string($str);

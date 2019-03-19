876f19febce9ec430eddfff716834b6bd03eae68
ghostrider070008 <ghostrider07008@yandex.ru>
2019-03-19 17:16:41 +0300

    решено 3 задание

--- zadanie3/index.php
+++ zadanie3/index.php
@@ -4,15 +4,12 @@
 <h1>Отзывы</h1>
 <form action='server.php' method="POST">
     <label for="name" class="for">Ваше имя:</label>
-    <input type="text" required placeholder="Ввкдите Ваше имя..." id="name" name="name_user"><br>
-    <label for="email" class="for" >Ваш e-mail:</label>
-    <input type="email" required placeholder="Ввкдите Ваш e-mail..." id="email" name="email_user"><br>
-    <label for="otziv" class="for">Ваш отзыв</label>
+    <input type="text" required placeholder="Введите Ваше имя..." id="name" name="name_user"><br>
+    <label for="otziv" class="for">Ваш отзыв:</label>
     <textarea type="textarea" required placeholder="Введите Ваш отзыв..." id="otziv" name="otziv_user"></textarea><br>
     <input type="image" src="img/otziv.png" id="submit_btn">
 </form>
 <div class="otziv_div">
-    
-
+<?include('otziv.php')?>
 </div>
 </main>

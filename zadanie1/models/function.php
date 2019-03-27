21caf1f7c5ed42f88d468735dbcc2daaa29e604f
ghostrider07008 <ghostrider07008@gmail.com>
2019-03-27 16:53:48 +0300

    3

--- zadanie1/models/function.php
+++ zadanie1/models/function.php
@@ -1,9 +1,23 @@
 <?
 require_once('../config.php');
-function addGoods_cart($id,$session){
+function addGoods_cart($id,$session,$id_buy){
     global $connect;
-    $date = date('j-m-Y H:i:s');
-    
+    $counts = 0;
+    //$date = date('j-m-Y H:i:s'); //получение текущей даты
+    if ($id_buy!==''){
+    $sql = "SELECT * FROM cart where id_goods=$id and id_buyers=$id_buy;";
+    $res = mysqli_query($connect,$sql);
+        if(mysqli_num_rows($res)=0){
+            $counts = 1;
+            $sql = "INSERT INTO cart (id_buyers, id_goods, count, dates) VALUES ($id_buy, $id, $counts, CURTIME());";
+        }
+        elseif(mysqli_num_rows($res)>0){
+            $counts++;
+            $sql = "UPDATE cart SET count=$counts, dates=CURTIME()  WHERE id_goods=$id and id_buyers=$id_buy;";
+        }
+    }
+
+    }
     $sql = "INSERT INTO cart (id_buyers, id_goods, count, dates, session_id) VALUES (1, $id, 1, CURTIME(),'$session');";
     echo $sql;
     print_r($connect);

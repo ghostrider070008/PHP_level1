4015b1918cbda58cf6859eaae9a158f7a5e49d20
ghostrider07008 <ghostrider07008@gmail.com>
2019-03-27 12:00:12 +0300

    шаг первый корзина. Определение с чего начать

---
+++ zadanie1/views/cart.php
@@ -0,0 +1,18 @@
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
+<link rel='stylesheet' href='css/style_cart.css' />
+<script src="js/jquery.js"></script>
+<h1>Корзина покупок</h1>
+<?
+//require_once('../config.php');
+/*
+$client  = @$_SERVER['HTTP_CLIENT_IP'];
+$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+$remote  = @$_SERVER['REMOTE_ADDR'];
+$clientip['client'] = $client;
+$clientip['forward'] = $forward;
+$clientip['remote'] = $remote;
+foreach($clientip as $key=>$value)
+    echo "$key => $value <br />";*/// как узнать ip адрес клиента
+   // print_r($connect);
+
+?>

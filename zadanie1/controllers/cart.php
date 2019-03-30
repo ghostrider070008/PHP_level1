253ddb87e9be53c7bac313a84111c59e6882f470
ghostrider070008 <ghostrider07008@yandex.ru>
2019-03-30 15:54:13 +0300

    6

--- zadanie1/controllers/cart.php
+++ zadanie1/controllers/cart.php
@@ -3,12 +3,14 @@ session_start();
 require_once('../config.php');
 $session = session_id();
 //echo $session;
-print_r($_SESSION);
-//$id_buy = 
+//print_r($_SESSION);
+$id_buy =$_SESSION['id_buy'];
+//echo "id_buyers=".$id_buy; 
 if ($id_buy == 0){
 $sql = "SELECT * FROM cart INNER JOIN tovar ON (cart.id_goods=tovar.id) where session_id='$session';";
 $res = mysqli_query($connect,$sql);
 $cart_content = '';
+$cart_content = '<div class="modal">';
 $cart_content .= '<div onclick="f_close_cart()"><p>X</p></div>';
 $cart_content .= '<h1>Корзина покупок</h1>';
     if (mysqli_num_rows($res)==0){
@@ -18,7 +20,7 @@ $cart_content .= '<h1>Корзина покупок</h1>';
 $cart_content .= '<table>';
 $counter = 1;
 while ($data = mysqli_fetch_assoc($res)){
- $cart_content .= '<tr><td>'.$counter.'</td><td>'.$data['title'].'</td><td>'.$data['count'].'</td><td>'.$data['price'].'</td></tr>';
+ $cart_content .= '<tr><td>'.$counter.'</td><td>'.$data['title'].'</td><td><button>+</button><p class="count">'.$data['count'].'</p><button>-</button></td><td>'.$data['price'].'</td></tr>';
  $counter++;
 }
 $cart_content .= '</table>';
@@ -40,7 +42,7 @@ while ($data = mysqli_fetch_assoc($res)){
  $cart_content .= '<tr><td>'.$counter.'</td><td>'.$data['title'].'</td><td>'.$data['count'].'</td><td>'.$data['price'].'</td></tr>';
  $counter++;
 }
-$cart_content .= '</table>';
+$cart_content .= '</table></div>';
 }
 };
 echo $cart_content;
